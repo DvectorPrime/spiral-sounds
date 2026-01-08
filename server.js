2a3ce9797e49f385906e558847f6562b019a46d6
DVectorprime <vectorprime2024@gmail.com>
2026-01-08 22:08:57 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import {productRouter} from "./routes/product.js"
+
+const PORT = 8000
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.use("/api/products", productRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+}).on('error', (err) => {
+  console.error('Failed to start server:', err)
+}) 
